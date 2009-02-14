1b1e487a2b46c272dabc64b48e84364022b3bd71
Fabio Kung <fabio.kung@gmail.com>
2009-02-14 01:10:18 -0200

    minor issue with some selections not being found in text. Thanks Peeja.

--- Support/bin/extract_method.rb
+++ Support/bin/extract_method.rb
@@ -8,10 +8,10 @@ $LOAD_PATH.unshift(bundle_lib) if ENV['TM_BUNDLE_SUPPORT'] and !$LOAD_PATH.inclu
 require 'text_mate'
 
 document = $stdin.read
-selected = ENV['TM_SELECTED_TEXT'].strip_blank_lines_at_end
+selected = ENV['TM_SELECTED_TEXT']
 index = document.index(selected)
 selected_start = document[0, index].count("\n") + 1
-selected_end = selected_start + selected.count("\n")
+selected_end = selected_start + selected.strip_blank_lines_at_end.count("\n")
 
 method_name = TextMate.input("New method name:", "new_method")
 
